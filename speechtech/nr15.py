7f6490dd836f96c6c89cff5de2f0269df6ae34af
Mike Hammond <hammond@u.arizona.edu>
2024-11-11 08:28:18 -0700

    nr15.py

---
+++ speechtech/nr15.py
@@ -0,0 +1,13 @@
+import math
+import matplotlib.pyplot as plt
+
+def gelu(x):
+	res = x * 1/2 * (1 + math.erf(x/(math.sqrt(2))))
+	return res
+
+v = [x/10 for x in range(-100,100)]
+w = list(map(gelu,v))
+
+plt.plot(v,w)
+plt.show()
+
