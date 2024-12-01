d73a639afc19fceda598258f8823369a1248cd87
Mike Hammond <hammond@u.arizona.edu>
2024-11-30 17:12:33 -0700

    ns2.py

--- speechtech/ns2.py
+++ speechtech/ns2.py
@@ -11,9 +11,10 @@ w2 = t.tensor([[[1.,1.,1.]]])
 #range of values to map from
 therange = t.arange(-5,5,.1)
 
+
 #iterate
 for i in therange:
-	inp = t.tensor([[i,i,i]])
+	inp = t.tensor([[[i,i,i]]])
 	#the math
 	res = t.tanh(t.conv1d(inp,w1)) * \
 		t.sigmoid(t.conv1d(inp,w2))
@@ -22,3 +23,4 @@ for i in therange:
 #plot results
 plt.plot(therange,allres)
 plt.show()
+
