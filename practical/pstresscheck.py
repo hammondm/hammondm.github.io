e405da7e4e9bdc4d4d84bd06527a5d87955edcbf
Mike Hammond <hammond@u.arizona.edu>
2026-09-19 08:10:07 -0400

    persian stress checking

---
+++ practical/pstresscheck.py
@@ -0,0 +1,63 @@
+from pyfoma import FST
+import pfeatures as pf
+
+#persian stress
+
+f = open('wikifasSTRESS.txt','r')
+t = f.read()
+f.close()
+
+lines = t.split('\n')[:-1]
+
+#drop everything up to tab
+drop = FST.re("$^rewrite((.*'\t'+):'',leftmost=True)")
+
+#push letters together
+spaces = FST.re("$^rewrite(' ':'')")
+
+#fix [g] problem
+grule = FST.re("$^rewrite(g:ɡ)")
+
+#eliminate syllable boundaries
+dotrule = FST.re("$^rewrite('.':'')")
+
+#fix t͡ʃ problem
+chrule = FST.re("$^rewrite((tʃ):(t͡ʃ))")
+
+#onset rule
+onset = FST.re(
+	"$sm? | $sm [mvrlɾw]",
+	{'sm':pf.sylminus}
+)
+
+#simple syllables
+syllable = FST.re(
+	"$o $sp $sm{0,2}",
+	{'sp':pf.sylplus,'sm':pf.sylminus,'o':onset}
+)
+
+#a word is one or more syllables
+word = FST.re(
+	"(ˈ? ($s):w)+",
+	{'s':syllable}
+)
+
+stress = FST.re(
+	"$^rewrite((ˈw):s)"
+)
+
+pat = FST.re(
+	".*s.*ww"
+)
+
+everything = FST.re(
+	"$d @ $s @ $g @ $dot @ $ch @ $w @ $str @ $pat",
+	{'d':drop,'s':spaces,'g':grule,'dot':dotrule,
+	'ch':chrule,'w':word,'str':stress,'pat':pat}
+)
+
+for line in lines:
+	res = set(everything.generate(line))
+	if len(res) > 0:
+		print(f'{line}\n\t{res}')
+
